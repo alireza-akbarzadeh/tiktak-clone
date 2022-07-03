fbda5105dd02eefd6748a316476d239e957d1492
AlirezaDev951 <devtools95@gmail.com>
2022-07-03 16:49:31 +0430

    feat:implement sidebar featurest

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -7,15 +7,29 @@ import { useRouter } from "next/router";
 import { topics } from "../utils/constants";
 
 const Discover = () => {
+  const router = useRouter();
+  const { topic: topicURl } = router.query;
+  const sameStyle =
+    "xl:border-2 hover:bg-primary cursor-pointer  px-3 py-2 rounded-md xl:rounded-4 flex items-center gap-2 justify-center";
+  const activeTopicStyle = `xl:border-[#f51997]=   text-[#f51997] ${sameStyle}`;
+  const topicStyle = `xl:border-gray-300  text-black ${sameStyle}`;
   return (
     <div className='xl:border-b-2 xl:border-gray-200 pb-6'>
-      <p className='text-gray-500 font-semibold m-3 mt-3 hidden xl:block'>
+      <p className='text-gray-500 font-semibold m-3 mt-3  xl:block'>
         <div className='flex gap-3 flex-wrap'>
           {topics.map((topic) => (
-            <Link key={`?topic=${topic.name}`} href={topic.name}>
-              {topic.name}
-              <div className=''>
-                <span>{topic.icon}</span>
+            <Link href={`?topic=${topic.name}`} key={topic.name}>
+              <div
+                className={
+                  topicURl === topic.name ? activeTopicStyle : topicStyle
+                }
+              >
+                <span className='font-bold text-2xl xl:text-md'>
+                  {topic.icon}
+                </span>
+                <span className='font-medium text-md hidden xl:block capitalize'>
+                  {topic.name}
+                </span>
               </div>
             </Link>
           ))}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,29 @@
+// @React
 import React from "react";
-
+// @utils
+import { footerList1, footerList2, footerList3 } from "../utils/constants";
+// @JSX
 const Footer = () => {
-  return <div>Footer</div>;
+  const List = ({ footerList }: { footerList: string[] }) => (
+    <div className='flex flex-wrap gap-2 mt-5'>
+      {footerList.map((footer) => (
+        <p
+          className='text-gray-4000 text-sm hover:underline cursor-pointer'
+          key={footer}
+        >
+          {footer}
+        </p>
+      ))}
+    </div>
+  );
+  return (
+    <div className='mt-6 hidden xl:block'>
+      <List footerList={footerList1} />
+      <List footerList={footerList2} />
+      <List footerList={footerList3} />
+      <p className='text-gray-400 text-sm mt-5'>2022 TikTik</p>
+    </div>
+  );
 };
 
 export default Footer;
